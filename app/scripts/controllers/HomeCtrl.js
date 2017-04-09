2e463dc920be2aa89b3694d53467604abf0bdb76
Tarek Saade <Tarek Saade>
2017-04-09 17:53:15 -0400

    Checkpoint 7 and project: Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,32 +1,22 @@
 (function() {
     'use strict'
     
-    function HomeCtrl($scope, $firebaseArray, $interval) {        
+    function HomeCtrl($scope, $firebaseArray, $interval, Tasks) {        
         var timerRef = firebase.database().ref().child('timer_tasks');        
         $scope.timer_tasks = $firebaseArray(timerRef);
 
         $scope.isRunning = false;
-        $scope.isBreakTIme = false;
-        $scope.button = "Start";
         $scope.taskTimer = 25 * 60;
         $scope.shortBreak = 5 * 60;
         $scope.longBreak = 30 * 60;
         $scope.countDown = $scope.taskTimer;
         $scope.longBreakCounter = 0;
+        $scope.tasks = Tasks.all;
         
         $scope.timerDing = new buzz.sound( "/assets/sounds/Ding.mp3", {
             preload: true
         });
-
-        $scope.addTask = function() {
-            var task = {
-                taskText: $scope.taskText,
-            };
-            //console.log(task); //this is a local variable and thus doesn't resolve the task issue!
-            $scope.timer_tasks.$add(task).then(function(timerRef) { 
-                $scope.taskText = '';
-            });
-        };
+        
         
         $scope.startTimer = function() {
             $scope.isRunning = true;
@@ -38,12 +28,13 @@
                 if ($scope.countDown == 0) {
                     $interval.cancel($scope.timerPromise);
                     $scope.isRunning = false;
+                    $scope.isWorkSession = false;
                     $scope.button = 'Start';
                     $scope.timerDing.play();
                 
                     if (!$scope.isBreakTime) {
                         $scope.longBreakCounter++;
-                        console.log($scope.longBreakCounter);
+                        $scope.addHistory();
                         $scope.isBreakTime = true;
                     
                         if ($scope.longBreakCounter % 4 === 0) {
@@ -61,15 +52,40 @@
             },1000);
         };
         
-        $scope.stopTimer = function(task) {
+        $scope.stopTimer = function() {
             $interval.cancel($scope.timerPromise);
             $scope.countDown = $scope.taskTimer;
             $scope.button = "Start"
             $scope.isRunning = false;
         };
+        
+        $scope.addHistory = function() {
+            var taskText = $('#text-field').val();
+            $scope.timer_tasks.$add({
+                taskText: taskText,
+                completed: Date.now()
+            });
+            $('#text-field').val('');
+        };
+        
+        $scope.clearTasks = function() {
+            Tasks.clearTasks();
+        }
     }
     
     angular
         .module('bloctime')
-        .controller('HomeCtrl', ['$scope', '$firebaseArray', '$interval', HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', '$firebaseArray', '$interval', 'Tasks', HomeCtrl]);
 })();
+
+/* Keep for furture reference:
+*        $scope.addTask = function() {
+*            var task = {
+*                taskText: $scope.taskText,
+*            };
+*            //console.log(task); //this is a local variable and thus doesn't resolve the task issue!
+*            $scope.timer_tasks.$add(task).then(function(timerRef) { 
+*                $scope.taskText = '';
+*            });
+*        };
+*/
